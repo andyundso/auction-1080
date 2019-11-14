d754201d6d19bf650a579a51a3b9df02f6de4e67
Andy Pfister <andy.pfister@hotmail.ch>
2019-11-15 00:35:55 +0100

    added new bids form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Col, Container, Row} from "reactstrap";
+import {Button, Col, Container, Form, FormGroup, Input, Label, Row} from "reactstrap";
 import firebase from './initializer/firebase';
 import {firestore} from 'firebase';
 
@@ -10,32 +10,72 @@ interface Bid {
   name: string,
 }
 
+interface NewBid {
+  bid: number
+  name: string
+}
+
 const App: React.FC = () => {
   const [bids, setBids] = useState<Bid[]>([]);
+  const [newBid, setNewBid] = useState<NewBid>({name: '', bid: 0});
+
+  async function getFirebaseDocs() {
+    const db = firebase.firestore();
+    const snapshot = await db.collection('bids').get();
+    const fireBaseBids = (snapshot.docs.map(doc => doc.data()) as Bid[]).sort((a, b) => b.bid - a.bid);
+
+    setBids(fireBaseBids);
+    setNewBid({...newBid, bid: fireBaseBids[0].bid + 1});
+  }
 
   useEffect(() => {
-    async function getFirebaseDocs() {
-      const db = firebase.firestore();
-      const snapshot = await db.collection('bids').get();
-      setBids(snapshot.docs.map(doc => doc.data()) as Bid[])
-    }
+    getFirebaseDocs()
+  }, []);
 
-    getFirebaseDocs();
-  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setNewBid({
+      ...newBid,
+      [e.target.name]: e.target.value,
+    });
 
-  return (
-    <Container>
+  const addBid = (e: React.MouseEvent<any, MouseEvent>) => {
+    e.preventDefault();
+
+    const db = firebase.firestore();
+    db.collection('bids').add({...newBid, created_at: firestore.Timestamp.now()});
+    getFirebaseDocs()
+  };
+
+  if (bids.length > 0) {
+    return <Container>
       <Row>
         <Col sm="12" md={{size: 6, offset: 3}}>
           <h1>OUHNDY'S 1080 AUKTION</h1>
 
-          {bids.sort((a, b) => b.bid - a.bid).map((bid: Bid) => (
+          <br/>
+
+          <Form>
+            <FormGroup>
+              <Label for={"name"}>Name</Label>
+              <Input type={"text"} name={"name"} placeholder={"Max Müller"} value={newBid.name} onChange={handleChange}/>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for={"bid"}>Dein Angebot</Label>
+              <Input type={"number"} min={bids[0].bid + 1} name={"bid"} value={newBid.bid} onChange={handleChange}/>
+            </FormGroup>
+
+            <Button onClick={addBid}>Einreichen</Button>
+          </Form>
+
+          <br/>
+
+          {bids.map((bid: Bid) => (
             <Row key={bid.bid}>
               <Col>
                 <h3>{bid.name}</h3>
               </Col>
               <Col className={"text-right"}>
-                <h3>{(bid.bid / 100).toFixed(2)} CHF</h3>
+                <h3>{bid.bid} CHF</h3>
                 <p>{bid.created_at.toDate().toLocaleDateString('de-CH')} {bid.created_at.toDate().toLocaleTimeString('de-CH', {
                   hour: "numeric",
                   minute: "numeric"
@@ -46,7 +86,9 @@ const App: React.FC = () => {
         </Col>
       </Row>
     </Container>
-  );
+  } else {
+    return <p>Einen Moment, die Seite wird geladen ..</p>;
+  }
 };
 
 export default App;
