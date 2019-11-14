fbd8a01a5ae739345d32c8a6edb41902fbde4ed3
Andy Pfister <andy.pfister@hotmail.ch>
2019-11-14 22:45:19 +0100

    Added basic installation of firebase, dotenv and bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Col, Container, Row} from "reactstrap";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <h1>OUHNDY'S 1080 AUKTION</h1>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
